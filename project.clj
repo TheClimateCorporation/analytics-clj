0fbe7bf35a2914753440825d6e3e35abca73f5ca
jeff.melching <jeff.melching@climate.com>
2015-02-02 15:18:03 -0600

    add climate deps to build and deploy

--- project.clj
+++ project.clj
@@ -1,12 +1,18 @@
-(defproject analytics-clj "0.3.0"
+(defproject analytics-clj "0.3.0-tcc"
   :description "Idiomatic Clojure wrapper for the segment.io Java client"
   :url "http://ardoq.com"
+  :repositories {"climate" "http://xdeps.ci.climatedna.net/maven"}
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :plugins [[codox "0.6.6"]]
   :codox {:src-linenum-anchor-prefix "L"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[codox "0.6.6"]
+                 [org.clojure/clojure "1.5.1"]
                  [com.github.segmentio/analytics "1.0.4"]]
+
+  :plugins [[com.climate.plugins/tcc-base-project "2014.10.27T23.49.27.ac58238"]
+            [com.climate.leiningen/lein-jenkins "000004.e9d2051"]]
+
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.13"]
                                   [ch.qos.logback/logback-core "1.0.13"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.5"]]}})
+                                  [org.slf4j/jcl-over-slf4j "1.7.5"]]}}
+  :aliases {"package" ["do" "clean," "jar"]})
